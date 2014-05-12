5a8d7368604d954a11adae5a574e8d2bacb09b89
esteban <esteban@esteban-System-Product-Name>
2014-05-12 13:31:40 -0300

    Subida de proyecto.

---
+++ grupo8/src/main/java/tp1_7510/grupo8/App.java
@@ -0,0 +1,13 @@
+package tp1_7510.grupo8;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
