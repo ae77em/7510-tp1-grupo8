4baa2275487891ef314c4ce0f0befd29fe925d50
esteban <esteban@esteban-System-Product-Name>
2014-05-13 19:25:16 -0300

    Modificaciones a clase configurer y main.

--- grupo8/src/main/java/tp1_7510/grupo8/Configurer.java
+++ grupo8/src/main/java/tp1_7510/grupo8/Configurer.java
@@ -1,7 +1,31 @@
 package tp1_7510.grupo8;
 
-public class Configurer {
-	
-	
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 
+public class Configurer {
+ 
+	 /**
+	  * Configura el logger desde un archivo properties.
+	  */
+	 public void configureFromProperties() {
+		 try {			 
+	   		Properties propiedades = new Properties();
+			    
+			propiedades.load(new FileInputStream("src/main/java/properties/logger.properties"));
+			 
+			String format = propiedades.getProperty("format");
+			String level = propiedades.getProperty("level");
+			
+			System.out.println("Formato: "+format+ "\n" +"Nivel: "+ level);
+	
+	 	} catch (FileNotFoundException e) {
+ 			System.out.println("Error, el archivo de propiedades no existe.");
+ 		} catch (IOException e) {
+			System.out.println("Error, no se puede leer el archivo");
+ 		}
+ 	}
 }
+
--- grupo8/src/main/java/tp1_7510/grupo8/Main.java
+++ grupo8/src/main/java/tp1_7510/grupo8/Main.java
@@ -3,7 +3,10 @@ package tp1_7510.grupo8;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("HOLA MUNDO, jajajaj");
+		
+		Configurer c = new Configurer();
+		
+		c.configureFromProperties();
 	}
 
 }
