7f59723f25c9beea49832f3462d7f73bc843c191
Federico <federicoferreyra60@yahoo.com.ar>
2014-05-13 18:12:50 -0300

    Se crea clase para darle formato al mensaje, se crea ciclo dentro del metodo log del Loguer para que imprima el mensaje

---
+++ grupo8/src/main/java/tp1_7510/grupo8/FormaterMessage.java
@@ -0,0 +1,10 @@
+package tp1_7510.grupo8;
+
+public class FormaterMessage {
+
+	public String formatMessage(String message, LogLevel level) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- grupo8/src/main/java/tp1_7510/grupo8/Loguer.java
+++ grupo8/src/main/java/tp1_7510/grupo8/Loguer.java
@@ -1,8 +1,27 @@
 package tp1_7510.grupo8;
 
-public class Loguer {
+import java.util.ArrayList;
 
-	public void log(String string, LogLevel level) {
-		// TODO Auto-generated method stub
+import tp1_7510.grupo8.Printer.Printer;
+
+public class Loguer {
+	
+	private ArrayList<Printer> m_Printers;
+	private FormaterMessage m_FormaterMessage;
+
+	public void log(String message, LogLevel level) {
+		String formatedMessage;
+        
+        if(isGreaterOrEqualThanLevelConfigured(level)){
+        	formatedMessage = m_FormaterMessage.formatMessage( message, level );
+                
+            for (int i = 0; i < m_Printers.size(); i++){
+            	m_Printers.get(i).print( formatedMessage );
+            }
+        }
+	}
+
+	private boolean isGreaterOrEqualThanLevelConfigured(LogLevel level) {
+		return true;
 	}
 }
