034b9e14b1764943ed61f86b640d1e79043aee14
Federico <federicoferreyra60@yahoo.com.ar>
2014-05-13 17:50:56 -0300

    se agregan clases Config,Loguer y Main

---
+++ grupo8/src/main/java/tp1_7510/grupo8/Loguer.java
@@ -0,0 +1,5 @@
+package tp1_7510.grupo8;
+
+public class Loguer {
+
+}
---
+++ grupo8/src/main/java/tp1_7510/grupo8/Main.java
@@ -0,0 +1,9 @@
+package tp1_7510.grupo8;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("HOLA MUNDO, jajajaj");
+	}
+
+}
