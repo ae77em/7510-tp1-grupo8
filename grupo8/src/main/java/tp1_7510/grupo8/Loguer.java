5eddf77f3ef2af371f4febd7c1a7258885f39477
Federico <federicoferreyra60@yahoo.com.ar>
2014-05-13 17:59:23 -0300

    Se crea ENUM para definir el nivel de Log

--- grupo8/src/main/java/tp1_7510/grupo8/Loguer.java
+++ grupo8/src/main/java/tp1_7510/grupo8/Loguer.java
@@ -2,4 +2,7 @@ package tp1_7510.grupo8;
 
 public class Loguer {
 
+	public void log(String string, LogLevel level) {
+		// TODO Auto-generated method stub
+	}
 }
--- grupo8/src/main/java/tp1_7510/grupo8/Main.java
+++ grupo8/src/main/java/tp1_7510/grupo8/Main.java
@@ -3,6 +3,10 @@ package tp1_7510.grupo8;
 public class Main {
 
 	public static void main(String[] args) {
+        Loguer loguer = new Loguer();
+        
+        loguer.log("Un mensaje DEBUG",LogLevel.DEBUG);
+
 		System.out.println("HOLA MUNDO, jajajaj");
 	}
 }
