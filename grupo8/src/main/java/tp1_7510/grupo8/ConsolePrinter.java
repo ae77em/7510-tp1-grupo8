9fa461ab12907b9fd5e53cda46e3542ab654646e
Federico <federicoferreyra60@yahoo.com.ar>
2014-05-13 10:28:14 -0300

    Se suben clases para imprimir los logs

---
+++ grupo8/src/main/java/tp1_7510/grupo8/ConsolePrinter.java
@@ -0,0 +1,11 @@
+public class ConsolePrinter implements Printer 
+{
+	ConsolePrinter(){
+		
+	}
+	
+	public void print(String message) 
+	{
+		System.out.println(message);
+	}
+}
---
+++ grupo8/src/main/java/tp1_7510/grupo8/FilePrinter.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class FilePrinter implements Printer 
+{
+	private PrintWriter m_Writter;
+	
+	FilePrinter(String filename) throws FileNotFoundException{
+		m_Writter = new PrintWriter(new FileOutputStream(new File(filename),true /* append = true */)); 
+	}
+	
+	public void print(String message){
+		m_Writter.println(message);
+	}
+}
