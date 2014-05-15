bb8d34329810870dfd168abda1c97a3d92ac6fd4
Federico <federicoferreyra60@yahoo.com.ar>
2014-05-15 08:51:45 -0300

    clase logger refactorizada

--- grupo8/src/main/java/tp1_7510/grupo8/Logger.java
+++ grupo8/src/main/java/tp1_7510/grupo8/Logger.java
@@ -12,6 +12,10 @@ import tp1_7510.grupo8.Printer.Printer;
 public class Logger {
 	private ArrayList<Printer> m_Printers = new ArrayList<Printer>();
 	
+	/*
+	 * El constructor toma un hash con las consolas y los archivos a donde loguear los mensajes
+	 * cada item de las lista contiene un hash que tiene la configuracion de cada LOG
+	 * */
 	Logger(Hashtable<String, ArrayList<Hashtable<String, String>>> dataConfiguration){
 		
 		m_Printers.addAll( createPrintersConsole(dataConfiguration.get("CONSOLES")));
@@ -19,6 +23,10 @@ public class Logger {
 		m_Printers.addAll( createPrintersFile(dataConfiguration.get("FILES")));
 	}
 	
+	/*
+	 * recorre la lista de FILES a donde loguear y lanza una instancia por cada archivo
+	 * 
+	 */
 	private ArrayList<Printer> createPrintersFile(ArrayList<Hashtable<String,String>> printersFiles) {
 		ArrayList<Printer> printers = new ArrayList<Printer>();
 		
@@ -33,18 +41,25 @@ public class Logger {
 		
 		return printers;
 	}
-
+	
+	/*
+	 * recorre la lista de CONSOLAS a donde loguear y lanza una instancia por cada consolas
+	 * 
+	 */
 	private ArrayList<Printer> createPrintersConsole(ArrayList<Hashtable<String, String>> printersConsole) {
 		ArrayList<Printer> printers = new ArrayList<Printer>();
 		
 		for(Hashtable<String, String> aConsolePrinter : printersConsole){
 			printers.add( new ConsolePrinter(aConsolePrinter) );
-		    //System.out.println(aConsolePrinter);
 		}
 		
 		return printers;
 	}
-
+	
+	/*
+	 *toma el mensaje a loguer y se lo pasa a cada printer para que aplique su respectivo formato y lo vuelque
+	 *a su respectiva salida 
+	 */
 	public void log(String message) {        
         for (int i = 0; i < m_Printers.size(); i++){
         	System.out.println(i);
@@ -52,6 +67,9 @@ public class Logger {
         }
 	}
 
+	/*
+	 * cierra todos los archivos, en el caso de las consolas llama al metodo close, pero no hace nada
+	 */
 	public void close() {
 		for (int i = 0; i < m_Printers.size(); i++){
           	m_Printers.get(i).close();
