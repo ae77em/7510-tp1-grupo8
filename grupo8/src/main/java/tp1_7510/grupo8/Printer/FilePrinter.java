6be59e247a00ff32843bccfe6554b8b78cb32508
esteban <esteban@esteban-System-Product-Name>
2014-05-15 11:26:27 -0300

    Agregado de diagrama de clases

--- grupo8/src/main/java/tp1_7510/grupo8/Printer/FilePrinter.java
+++ grupo8/src/main/java/tp1_7510/grupo8/Printer/FilePrinter.java
@@ -1,20 +1,52 @@
 package tp1_7510.grupo8.Printer;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Hashtable;
 
-public class FilePrinter implements Printer 
-{
+import tp1_7510.grupo8.Patterns.Pattern;
+import tp1_7510.grupo8.Patterns.PatternDate;
+import tp1_7510.grupo8.Patterns.PatternEscape;
+import tp1_7510.grupo8.Patterns.PatternFilename;
+import tp1_7510.grupo8.Patterns.PatternLevel;
+import tp1_7510.grupo8.Patterns.PatternLineNumber;
+import tp1_7510.grupo8.Patterns.PatternMethodName;
+import tp1_7510.grupo8.Patterns.PatternSeparator;
+import tp1_7510.grupo8.Patterns.PatternSimpleMessage;
+import tp1_7510.grupo8.Patterns.PatternThread;
+import tp1_7510.grupo8.Patterns.PatternUserDefinedMessage;
+
+public class FilePrinter extends Printer{
+
 	private PrintWriter m_Writter;
 	
-	FilePrinter(String filename) throws FileNotFoundException{
-		m_Writter = new PrintWriter(new FileOutputStream(new File(filename),true /* append = true */)); 
+	/*
+	 * recibe la configuracion de un archivo, lo crea y le pasa las reglas a la clase
+	 * factoryPattern para que las instancie
+	 */
+	public FilePrinter(Hashtable<String,String> dataConfiguration) throws FileNotFoundException{
+		super(dataConfiguration);
+		
+		m_Writter = new PrintWriter(new FileOutputStream(new File(dataConfiguration.get("name"))));
 	}
 	
-	public void print(String message){
-		m_Writter.println(message);
-	}
+	/*
+	 * formatee el texto y lo baja a disco
+	 */
+	public void print(String aMessage){		
+		String messageFormated = "";
 
-	public String notifyDestiny(String s) {
-		return s;
+		for(Pattern aPattern : m_messagePatterns){
+			messageFormated = aPattern.formatText(messageFormated);
+		}
+		
+		m_Writter.println(messageFormated);
+	}
+	
+	/*
+	 * cierra el archivo
+	 */
+	public void close() {
+		m_Writter.close();
 	}
 }
--- grupo8/src/main/java/tp1_7510/grupo8/Printer/Printer.java
+++ grupo8/src/main/java/tp1_7510/grupo8/Printer/Printer.java
@@ -1,6 +1,63 @@
 package tp1_7510.grupo8.Printer;
 
-public interface Printer {
-	void print(String s);
-	String notifyDestiny(String s);
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+import tp1_7510.grupo8.LogLevel;
+import tp1_7510.grupo8.Patterns.Pattern;
+import tp1_7510.grupo8.Patterns.PatternDate;
+import tp1_7510.grupo8.Patterns.PatternLevel;
+import tp1_7510.grupo8.Patterns.PatternThread;
+
+//interface que usan las clases que imprimen los mensajes de log
+public  class Printer {
+	protected ArrayList<Pattern> m_messagePatterns;//contendra los patrones a aplicar al mensaje
+	protected FactoryPatterns m_factoryPatterns;//fabrica los patrones a aplicar al mensaje
+	LogLevel m_levelLog;
+	
+	public Printer(Hashtable<String,String> dataConfiguration){
+		m_factoryPatterns = new FactoryPatterns(dataConfiguration);
+		
+		m_messagePatterns = m_factoryPatterns.createListOfPatterns();
+		
+		m_levelLog = setLogLevel(dataConfiguration.get("logLevel"));
+	}
+	
+	private LogLevel setLogLevel(String levelString){
+		LogLevel level;
+		
+        switch(levelString){
+		 case "OFF": 
+		     level = LogLevel.OFF;
+		     break;
+		 case "FATAL": 
+			 level = LogLevel.FATAL;
+		     break;
+		 case "ERROR": 
+			 level = LogLevel.ERROR;
+			 break;
+		 case "WARN": 
+			 level = LogLevel.WARN;
+		     break;
+		 case "INFO": 
+			 level = LogLevel.INFO;
+		     break;
+		 default: 
+			 level = LogLevel.DEBUG;
+			 break;
+		}
+        return level;
+	}
+
+	public void print(String s){
+		
+	}
+	
+	public void close(){
+		
+	}
+	
+	public Boolean verifyLogLevel(LogLevel level){
+		return (level.ordinal() >= m_levelLog.ordinal());
+	}
 }
