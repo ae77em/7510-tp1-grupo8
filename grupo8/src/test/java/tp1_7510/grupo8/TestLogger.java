d246e7ab10e8d0e92461bf12dbd43f61a2c4fae3
Federico <federicoferreyra60@yahoo.com.ar>
2014-05-15 21:11:03 -0300

    se sube documentacion

--- grupo8/src/test/java/tp1_7510/grupo8/TestLogger.java
+++ grupo8/src/test/java/tp1_7510/grupo8/TestLogger.java
@@ -1,31 +1,65 @@
 package tp1_7510.grupo8;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 import junit.framework.TestCase;
 
 public class TestLogger extends TestCase {
+	//EN EL CASO DE HABER ERROES SE LOGUEA EL error al archivo erroe.
+	public void testOkLogMessage(){ //a causa de nivel de mensaje
+		String messageError = "";
+		String lineaArchivo = "";
+		Configurer configurer = new Configurer();
 
-	Configurer configurer = new Configurer();
-	
-	/*configurer.createPrinter("files","file4.txt");
-	configurer.createPrinter("files","file2.txt");
-	
-	configurer.setFormatDate("file4.txt","%d{yyyyy-mm-dd hh:mm:ss}");
-	configurer.setFormatMessage("file4.txt","%L-%n-%d{HH:mm:ss}-%n-%m-%n-%p-%n-%t-%n-%F-%M");
-	configurer.setLogLevel("file4.txt","ERROR");
-	configurer.setSeparator("file4.txt","*");
-	
-	configurer.setFormatDate("file2.txt","%d{hh:mm:ss}");
-	configurer.setFormatMessage("file2.txt","%d{hh:mm:ss}-%n-%m-%n-%p-%n-%t-%n-%F-MENSAJEFEO");
-	configurer.setLogLevel("file2.txt","FATAL");
-	
-	configurer.createPrinter("consoles","SUPER_CONSOLA");
-	configurer.setLogLevel("SUPER_CONSOLA","FATAL");
-	configurer.setFormatMessage("consoleDefault","	-%n-%p-%n-%t-%n-%F-%M-%t");
-	//configurer.setFormatMessage("SUPER_CONSOLA","%d{HH:mm:ss}-%n-%m-%n-%p-%n-%t-%n-%F-%M-%t-%%-hola");
-	
-	configurer.eraseDefaultConsole();*/
-	
-	Logger loguer = new Logger( configurer.getPrintersConfiguration() );
-    		
-    //loguer.log("Mensaje1",LogLevel.ERROR);
+		configurer.createPrinter("files","file4.txt");
+		configurer.setLogLevel("file4.txt","DEBUG");
+		configurer.eraseDefaultConsole();
+		
+		Logger loguer = new Logger( configurer.getPrintersConfiguration() );	
+		
+		loguer.log("Mensaje1",LogLevel.FATAL);
+		
+		loguer.close();
+		
+		try {
+			lineaArchivo = new BufferedReader( new FileReader ( new File ("error.dat") ) ).readLine();
+			if (lineaArchivo==null) lineaArchivo ="";
+			//si devuelve null es poque es archivo estaba vacio,
+			//si el archivo esta vacio es porque no se registraron errores
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		assertEquals(lineaArchivo,messageError);
+	};
+
+	//se configura la impresora para imprimir mensajes de nivel FATAL, pero se intenta logear un DEBUG
+	public void testErrorLogMessage(){ //a causa de nivel de mensaje
+		String messageError = "Error in Printer: file4.txt levelPrinter: FATAL levelMessage DEBUG";
+		String lineaArchivo = "";
+		Configurer configurer = new Configurer();
+
+		configurer.createPrinter("files","file4.txt");
+		configurer.setLogLevel("file4.txt","FATAL");
+		configurer.eraseDefaultConsole();
+		
+		Logger loguer = new Logger( configurer.getPrintersConfiguration() );	
+		
+		loguer.log("Mensaje1",LogLevel.DEBUG);
+		
+		loguer.close();
+		
+		try {
+			lineaArchivo = new BufferedReader( new FileReader ( new File ("error.dat") ) ).readLine();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		assertEquals(lineaArchivo,messageError);
+	};
 }
