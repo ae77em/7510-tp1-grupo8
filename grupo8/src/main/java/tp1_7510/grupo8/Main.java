ade94ca35c778d11689791396540557ed334ffcb
Federico <federicoferreyra60@yahoo.com.ar>
2014-05-29 18:27:23 -0300

    casi terminado faltan alguns test

--- grupo8/src/main/java/tp1_7510/grupo8/Main.java
+++ grupo8/src/main/java/tp1_7510/grupo8/Main.java
@@ -3,25 +3,7 @@ package tp1_7510.grupo8;
 public class Main{
 
 	public static void main(String[] args) {
-		Configurator configurator = new Configurator();
-		
-		configurator.createPrinter("files","file4.txt");
-		configurator.createPrinter("files","file2.txt");
-		
-		configurator.setFormatDate("file4.txt","%d{yyyyy-mm-dd hh:mm:ss}");
-		configurator.setFormatMessage("file4.txt","%L-%n-%d{HH:mm:ss}-%n-%m-%n-%p-%n-%t-%n-%F-%M");
-		configurator.setLogLevel("file4.txt",LogLevel.ERROR);
-		configurator.setSeparator("file4.txt","*");
-		
-		configurator.setFormatDate("file2.txt","%d{hh:mm:ss}");
-		configurator.setFormatMessage("file2.txt","%d{hh:mm:ss}-%n-%m-%n-%p-%n-%t-%n-%F-MENSAJEFEO");
-		configurator.setLogLevel("file2.txt",LogLevel.FATAL);
-		
-		configurator.createPrinter("consoles","SUPER_CONSOLA");
-		configurator.setLogLevel("SUPER_CONSOLA",LogLevel.FATAL);
-		configurator.setFormatMessage("SUPER_CONSOLA","%d{HH:mm:ss}-%n-%m-%n-%p-%n-%t-%n-%F-%M-%t-%%-hola");
-		
-		configurator.eraseDefaultConsole();
+		Configurator configurator = new Configurator("src/main/java/tp1_7510/grupo8/Properties/logger.properties");
 		
 		Logger logger = new Logger( configurator.getPrintersConfiguration() );
         		
--- grupo8/src/test/java/tp1_7510/grupo8/TestConfigurer.java
+++ grupo8/src/test/java/tp1_7510/grupo8/TestConfigurer.java
@@ -8,76 +8,8 @@ import junit.framework.TestCase;
 public class TestConfigurer 
     extends TestCase{
 	
-	private Configurator configurator = new Configurator();
-		
-	public void testCreateConsolesConfigurer(){
-		configurator.createPrinter("consoles","console1");
-		configurator.createPrinter("consoles","console2");
-		
-		assertEquals("consoleDefault,console1,console2,", configurator.getConsoles());
-	}
-	
-	public void testCreateFilesConfigurer(){
-		configurator.createPrinter("files","file1");
-		configurator.createPrinter("files","file2");
-		
-		assertEquals("file1,file2,", configurator.getFiles());
-	}
-	
-	public void testDefaultSettingsFile(){
-		configurator.createPrinter("files","file3");
-		
-		assertEquals(configurator.getFormatDate("file3"),"%d{HH:mm:ss}");
-		assertEquals(configurator.getFormatMessage("file3"),"%d{HH:mm:ss}-%p-%t-%m");
-		assertEquals(configurator.getLogLevel("file3"),LogLevel.DEBUG);
-		assertEquals(configurator.getSeparator("file3"),"-");
-	}
-	
-	public void testChangeDefaultSettingsFile(){
-		configurator.createPrinter("files","file4");
-		
-		configurator.setFormatDate("file4","%d{yyyyy-mm-dd hh:mm:ss}");
-		configurator.setFormatMessage("file4","%d{HH:mm:ss}-%t-%p-%T-%m");
-		configurator.setLogLevel("file4",LogLevel.ERROR);
-		configurator.setSeparator("file4","*");
-		
-		assertEquals(configurator.getFormatDate("file4"),"%d{yyyyy-mm-dd hh:mm:ss}");
-		assertEquals(configurator.getFormatMessage("file4"),"%d{HH:mm:ss}-%t-%p-%T-%m");
-		assertEquals(configurator.getLogLevel("file4"),LogLevel.ERROR);
-		assertEquals(configurator.getSeparator("file4"),"*");	
-	}
-	
-	public void testGetFileConfiguration(){
-		Hashtable<String,String> hashAux = new Hashtable<String,String>();
-		hashAux.put("separator","-");
-		hashAux.put("format","%d{HH:mm:ss}-%p-%t-%m");
-		hashAux.put("name","file5");
-		hashAux.put("formatDate","%d{HH:mm:ss}");
-		hashAux.put("logLevel",LogLevel.DEBUG.toString());
-		
-		configurator.createPrinter("files","file5");
+	//private Configurator configurator = new Configurator();
 			
-		assertEquals(configurator.getPrinterConfiguration("file5"),hashAux);		
-	}
-
-	
-	public void testGetConfigurationPrinters(){
-		configurator.createPrinter("files","file5");
-		configurator.createPrinter("files","file6");
-		configurator.createPrinter("files","file7");
-		
-		configurator.createPrinter("consoles","console5");
-		configurator.createPrinter("consoles","console6");
-		configurator.createPrinter("consoles","console7");
-		
-		Hashtable<String,ArrayList<Hashtable<String, String>>> printers = new Hashtable<String,ArrayList<Hashtable<String, String>>>();
-		
-		printers.put("FILES", configurator.getPrintersConfiguration("files"));
-		printers.put("CONSOLES", configurator.getPrintersConfiguration("consoles"));
-
-		assertTrue(true);
-	}
-	
 /*	public void testLogConsoles(){
 		
 		configurator.createPrinter("consoles","aConsole");
