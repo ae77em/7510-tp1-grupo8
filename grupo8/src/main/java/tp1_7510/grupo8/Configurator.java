7c1ca72d9db68c197c2f0e0e1871305a0e24346c
esteban <esteban@esteban-System-Product-Name>
2014-05-29 19:04:01 -0300

    Se cambió el nombre del test suite del configurator

--- grupo8/src/main/java/tp1_7510/grupo8/Configurator.java
+++ grupo8/src/main/java/tp1_7510/grupo8/Configurator.java
@@ -1,10 +1,8 @@
 package tp1_7510.grupo8;
 
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.Properties;
