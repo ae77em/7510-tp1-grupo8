36b511d404632c06cc33dabac1442459627ff004
Federico <federicoferreyra60@yahoo.com.ar>
2014-06-05 00:20:18 -0300

    se sube ejemploe de config JSON

---
+++ grupo8/src/main/java/org/slf4j/impl/LoggerFactory.java
@@ -0,0 +1,27 @@
+package org.slf4j.impl;
+ 
+import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+ 
+import java.util.HashMap;
+import java.util.Map;
+ 
+ 
+public class LoggerFactory implements ILoggerFactory {
+    private Map<String, LoggerAdapter> loggerMap;
+ 
+    public LoggerFactory() {
+        loggerMap = new HashMap<String, LoggerAdapter>();
+    }
+ 
+    @Override
+    public Logger getLogger(String name) {
+        synchronized (loggerMap) {
+            if (!loggerMap.containsKey(name)) {
+                loggerMap.put(name, new LoggerAdapter(name));
+            }
+ 
+            return loggerMap.get(name);
+        }
+    }
+}
