da83e4f4a2f5791dfffd315e6df8c7fa070ea260
Oscar <fferreyra38@gmail.com>
2014-06-18 20:43:08 -0300

    por algun maldito motivo no se puede subir el main, se sube todo comentado a MAINA

--- grupo8/src/main/java/tp1_7510/grupo8/Maina.java
+++ grupo8/src/main/java/tp1_7510/grupo8/Maina.java
@@ -1,4 +1,28 @@
 package tp1_7510.grupo8;
 
-public class Maina {
-}
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import tp1_7510.grupo8.Logger.Logger;
+
+/*public class Main {
+
+	public static void main(String[] args) {
+		Configurator configurator = new Configurator();
+		
+		JSONArray jsonArrayConfigLogger = configurator.getConfigurationLogger();
+		
+		//solo para probar como se cargan las configuraciones
+		System.out.println((JSONObject)jsonArrayConfigLogger.get(0));
+		System.out.println((JSONObject)jsonArrayConfigLogger.get(1));
+		System.out.println((JSONObject)jsonArrayConfigLogger.get(2));
+		System.out.println((JSONObject)jsonArrayConfigLogger.get(3));
+		
+		Logger aLogger = new Logger((JSONObject)jsonArrayConfigLogger.get(2));
+		
+		aLogger.error("mensajeERROR");
+		
+		aLogger.close();
+	}
+
+}*/
