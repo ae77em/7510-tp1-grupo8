6534c632fb877a06e3b9e85721040d17b250397d
Oscar <fferreyra38@gmail.com>
2014-06-18 20:14:47 -0300

    se corrigen test de XML loader

--- grupo8/src/test/java/tp1_7510/grupo8/TestLoaderProperties.java
+++ grupo8/src/test/java/tp1_7510/grupo8/TestLoaderProperties.java
@@ -6,63 +6,51 @@ import junit.framework.TestCase;
 
 public class TestLoaderProperties extends TestCase {
 
-	String path = "src/main/java/tp1_7510/grupo8/Config/test/config.properties";
+	String path = "src/test/java/tp1_7510/grupo8/testOutputFiles/testConfig/config.properties";
 
 	public LoaderPropertiesConfiguration loaderPropertiesConfiguration = new LoaderPropertiesConfiguration(path);
 	
 	public void testLengthLoggers() {
-		assertEquals(2,loaderPropertiesConfiguration.getLenghtLoggers());
+		assertEquals(1,loaderPropertiesConfiguration.getLenghtLoggers());
 	}
 
 	public void testNameLoggers(){
-		System.out.println(loaderPropertiesConfiguration.getNameLogger(0));
 		assertEquals("filer1",loaderPropertiesConfiguration.getNameLogger(0));
-		assertEquals("filer2",loaderPropertiesConfiguration.getNameLogger(1));
 	}
 	
 	public void testTypeLoggers(){
-		assertEquals("file",loaderPropertiesConfiguration.getTypeLogger(0));
-		assertEquals("file",loaderPropertiesConfiguration.getTypeLogger(1));
+		assertEquals("FILES",loaderPropertiesConfiguration.getTypeLogger(0));
 	}
 	
 	public void testFileNameLoggers(){
 		assertEquals("aFileName1",loaderPropertiesConfiguration.getFileNameLogger(0));
-		assertEquals("aFileName2",loaderPropertiesConfiguration.getFileNameLogger(1));
 	}
 	
 	public void testLevelLoggers(){
 		assertEquals("DEBUG",loaderPropertiesConfiguration.getLevelLogLogger(0));
-		assertEquals("TRACE",loaderPropertiesConfiguration.getLevelLogLogger(1));
 	}
 	
 	public void testFormatDateLoggers(){
 		assertEquals("dd-M-yyyy hh:mm:ss",loaderPropertiesConfiguration.getFormatDateLogger(0));
-		assertEquals("yyyy MMM dd",loaderPropertiesConfiguration.getFormatDateLogger(1));
 	}
 	
 	public void testPatternMessageLoggers(){
-		assertEquals("%d%s%l%s",loaderPropertiesConfiguration.getPatternMessageLogger(0));
-		assertEquals("%d%s%l%s%m",loaderPropertiesConfiguration.getPatternMessageLogger(1));
+		assertEquals("%d-%n-%p-%n-%t-%n-%m",loaderPropertiesConfiguration.getPatternMessageLogger(0));
 	}
 	
 	public void testSeparatorLoggers(){
 		assertEquals("-",loaderPropertiesConfiguration.getSeparatorLogger(0));
-		assertEquals("*",loaderPropertiesConfiguration.getSeparatorLogger(1));
 	}
 	
 	public void testRegularExpresionLoggers(){
-		assertEquals("^[a-zA-Z0-9]*$",loaderPropertiesConfiguration.getRegularExpresionLogger(0));
-		assertEquals("^[a-z0-9]*$",loaderPropertiesConfiguration.getRegularExpresionLogger(1));
+		assertEquals("^[a-zA-Z[:space:]]*$",loaderPropertiesConfiguration.getRegularExpresionLogger(0));
 	}
 	
 	public void testCustomFilterLoggers(){
 		Hashtable<String, String> hashCustomsFilters = new Hashtable<String, String>();
 		
-		hashCustomsFilters.put("%d","filter%d");
-		hashCustomsFilters.put("%s","filter%s");
-		hashCustomsFilters.put("%l","filter%l");
-		hashCustomsFilters.put("%T","filter%T");
-
+		hashCustomsFilters.put("%p","DEBUG");
+		
 		assertEquals(hashCustomsFilters,loaderPropertiesConfiguration.getCustomFilterLogger(0));
 	}
 }
--- grupo8/src/test/java/tp1_7510/grupo8/TestLoaderXml.java
+++ grupo8/src/test/java/tp1_7510/grupo8/TestLoaderXml.java
@@ -1,12 +1,13 @@
 package tp1_7510.grupo8;
 
 import java.util.Hashtable;
+
 import junit.framework.TestCase;
 
 
 public class TestLoaderXml extends TestCase{
-	String path = "src/main/java/tp1_7510/grupo8/Config/test/config.xml";
-
+	String path = "src/test/java/tp1_7510/grupo8/testOutputFiles/testConfig/config.xml";
+	
 	public LoaderXmlConfiguration loaderXmlConfiguration = new LoaderXmlConfiguration(path);
 	
 	public void testLengthLoggers() {
@@ -15,57 +16,43 @@ public class TestLoaderXml extends TestCase{
 
 	public void testNameLoggers(){
 		assertEquals("LogParser",loaderXmlConfiguration.getNameLogger(0));
-		assertEquals("LogIndexer",loaderXmlConfiguration.getNameLogger(1));
 	}
 	
 	public void testTypeLoggers(){
-		assertEquals("FILES",loaderXmlConfiguration.getTypeLogger(0));
-		assertEquals("CONSOLES",loaderXmlConfiguration.getTypeLogger(1));
+		assertEquals("CONSOLES",loaderXmlConfiguration.getTypeLogger(0));
 	}
 	
 	public void testFileNameLoggers(){
 		assertEquals("parserLog",loaderXmlConfiguration.getFileNameLogger(0));
-		assertEquals("indexerLog",loaderXmlConfiguration.getFileNameLogger(1));
 	}
 	
 	public void testLevelLoggers(){
 		assertEquals("DEBUG",loaderXmlConfiguration.getLevelLogLogger(0));
-		assertEquals("ERROR",loaderXmlConfiguration.getLevelLogLogger(1));
 	}
 	
 	public void testFormatDateLoggers(){
 		assertEquals("dd-M-yyyy hh:mm:ss",loaderXmlConfiguration.getFormatDateLogger(0));
-		assertEquals("yyyy MMM dd",loaderXmlConfiguration.getFormatDateLogger(1));
 	}
 	
 	public void testPatternMessageLoggers(){
-		assertEquals("%d%s%l%s",loaderXmlConfiguration.getPatternMessageLogger(0));
-		assertEquals("%L%s%d%s%l%s",loaderXmlConfiguration.getPatternMessageLogger(1));
+		assertEquals("%L-%n-%d-%n-%g-%n-%p-%n-%m",loaderXmlConfiguration.getPatternMessageLogger(0));
 	}
 	
 	public void testSeparatorLoggers(){
 		assertEquals("-",loaderXmlConfiguration.getSeparatorLogger(0));
-		assertEquals("*",loaderXmlConfiguration.getSeparatorLogger(1));
 	}
 	
 	public void testRegularExpresionLoggers(){
-		assertEquals("Expresion1",loaderXmlConfiguration.getRegularExpresionLogger(0));
-		assertEquals("Expresion2",loaderXmlConfiguration.getRegularExpresionLogger(1));
+		assertEquals("^[a-zA-Z0-9]*$",loaderXmlConfiguration.getRegularExpresionLogger(0));
 	}
 	
 	public void testCustomFilterLoggers(){
 		Hashtable<String, String> hashCustomsFilters = new Hashtable<String, String>();
 		
-		hashCustomsFilters.put("%d","patter1.1");
-		hashCustomsFilters.put("%s","patter1.2");
-		hashCustomsFilters.put("%l","patter1.3");
-		hashCustomsFilters.put("%T","patter1.4");
-
+		hashCustomsFilters.put("%L","^\\d+$");
+		hashCustomsFilters.put("%g","LogParser");
+		hashCustomsFilters.put("%m","^*patter14");
+		
 		assertEquals(hashCustomsFilters,loaderXmlConfiguration.getCustomFilterLogger(0));
-	}
-	
-	public void testLogLevelLoggers(){
-		assertEquals("DEBUG",loaderXmlConfiguration.getLevelLogLogger(0));
-		assertEquals("ERROR",loaderXmlConfiguration.getLevelLogLogger(1));
 	}
 }
